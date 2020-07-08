5df512b61731cc5183f308324c5446c7d6f69449
makro1 <termicab@gmail.com>
2020-07-08 17:31:20 +0200

    sql
    added userlogin and adminlogin connection with db

--- ElibraryManagment/ElibraryManagment/userlogin.aspx.cs
+++ ElibraryManagment/ElibraryManagment/userlogin.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,45 @@ namespace ElibraryManagment
 {
     public partial class userlogin : System.Web.UI.Page
     {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if(con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("select * from member_master_tbl where member_id='"+TextBox1.Text.Trim()+"' AND password ='" + TextBox2.Text.Trim() + "'", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Response.Write("<script>alert('Welcome "+dr.GetValue(8).ToString()+"'); </script>");
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid user!'); </script>");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+
+
+            // Response.Write("<script>alert('Sign Up Successful. Go to User Login to Login'); </script>");
         }
     }
 }
